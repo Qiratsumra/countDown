077d316f40ea0a3e7a9123148c89535a203923df
Qiratsumra <saeedqirat43@gmail.com>
2024-07-09 22:28:10 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from 'chalk';
+
+
+console.log(chalk.bgMagenta.bold(`>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>`));
+console.log(chalk.bgMagenta.bold(`<<<<<<<<<<<<<<<<<<<<<<     Welcome to the Count Down Timer     >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>`));
+console.log(chalk.bgMagenta.bold(`<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<`));
+
+
+
+let anwser =await inquirer.prompt(
+    [
+        {
+            name:"userAnswer",
+            type:"number",
+            message:"Enter your timer in the second"
+        }
+    ]
+);
+let {userAnswer} = anwser;
+
+async function start() {
+    async function startNow() {
+        if (userAnswer>=0) {
+            setTimeout(startNow,1000);
+            console.log(chalk.bgGreen.whiteBright(userAnswer));
+            userAnswer--
+            if (userAnswer===-1) {
+                setTimeout(startNow,1000);
+                console.log(chalk.bgRed(`Thanks for using`))
+                
+            }
+        }
+    };
+    startNow()
+};
+start();
